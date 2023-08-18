322cb1c19b85c09f6ac59f2c8a3e28138a5d8c46
mjlee111 <menggu1234@naver.com>
2023-08-18 14:50:01 +0900

    first commit

---
+++ include/qthread_ros/main_window.hpp
@@ -0,0 +1,40 @@
+/**
+ * @file /include/qthread_ros/main_window.hpp
+ *
+ * @brief Qt based gui for qthread_ros.
+ **/
+#ifndef qthread_ros_MAIN_WINDOW_H
+#define qthread_ros_MAIN_WINDOW_H
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QMainWindow>
+#include "ui_main_window.h"
+#include "qnode.hpp"
+
+/*****************************************************************************
+** Namespace
+*****************************************************************************/
+
+namespace qthread_ros
+{
+	class MainWindow : public QMainWindow
+	{
+		Q_OBJECT
+
+	public:
+		MainWindow(int argc, char **argv, QWidget *parent = 0);
+		~MainWindow();
+
+	public Q_SLOTS:
+
+	private:
+		Ui::MainWindowDesign ui;
+		QNode qnode;
+	};
+
+} // namespace qthread_ros
+
+#endif // qthread_ros_MAIN_WINDOW_H
